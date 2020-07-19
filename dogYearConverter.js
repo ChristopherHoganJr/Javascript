7e49f5801431f79569e16500dd52fcbd9a31ba8a
ChristopherHoganJr <55262451+ChristopherHoganJr@users.noreply.github.com>
2020-07-19 07:53:49 -0700

    Human years to dog year converter

---
+++ dogYearConverter.js
@@ -0,0 +1,14 @@
+var myAge = 30; // My age
+let earlyYears = 2; // Assigning early years the value of 2
+earlyYears *= 10.5; // Multiplying earlyYears by 10.5
+let laterYears = myAge - 2 // Assigning laterYears the value of myAge subtracted by 2
+laterYears *= 4; // Multiplying laterYears by 4
+
+console.log(earlyYears);
+console.log(laterYears);
+
+var myAgeInDogYears = earlyYears + laterYears; // Adding earlyYears and laterYears together
+
+var myName = 'Chris'.toLowerCase() // Formatting my name all lower case
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} in dog years.`) // Showing name, human age, and dog age output to the user
